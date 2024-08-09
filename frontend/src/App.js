ff78b8b36e797ee6aaf8c99000ea7f3f2a7e75da
Ryzen720 <guilhermedsmarta@gmail.com>
2024-08-09 13:32:52 -0300

    semHome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Cadastro from './pages/Cadastro/Cadastro';
 import Dashboard from './pages/Dashboard/Dashboard';
-import Home from './pages/Home/home';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />}></Route>
+        
         <Route path='/Login' element={<Login />}></Route>
         <Route path='/Cadastrar' element={<Cadastro />}></Route>
         <Route path='/dashboard' element={<Dashboard />}></Route>
